f9dbfc3ba955e3e5dd4e88c8467509f5899155c4
Anthony Burzillo <burz@jhu.edu>
2013-12-25 23:00:50 -0500

    Finish Matrix implementation (still a few bugs)

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <math.h>
+#include <stdio.h>
 
 #include "Matrix.h"
 
@@ -14,6 +15,17 @@ Matrix3::Matrix3(void)
   m[2][0] = 0;
   m[2][1] = 0;
   m[2][2] = 0;
+}
+
+Matrix3 Matrix3::Identity(void)
+{
+  Matrix3 result;
+
+  result(0, 0) = 1;
+  result(1, 1) = 1;
+  result(2, 2) = 1;
+
+  return result;
 }
 
 double& Matrix3::operator()(int x, int y)
@@ -158,30 +170,30 @@ Matrix3 Matrix3::operator*(const Matrix3& n) const
 {
   Matrix3 result;
 
-  result(0, 0) = m[0][0] * n(0, 0) + m[0][1] * n(1, 0) + m[0][2] * n(2, 0);
-  result(0, 1) = m[1][0] * n(0, 1) + m[1][1] * n(1, 1) + m[1][2] * n(2, 1);
-  result(0, 2) = m[2][0] * n(0, 2) + m[2][1] * n(1, 2) + m[2][2] * n(2, 2);
-  result(1, 0) = m[0][0] * n(0, 0) + m[0][1] * n(1, 0) + m[0][2] * n(2, 0);
-  result(1, 1) = m[1][0] * n(0, 1) + m[1][1] * n(1, 1) + m[1][2] * n(2, 1);
-  result(1, 2) = m[2][0] * n(0, 2) + m[2][1] * n(1, 2) + m[2][2] * n(2, 2);
-  result(2, 0) = m[0][0] * n(0, 0) + m[0][1] * n(1, 0) + m[0][2] * n(2, 0);
-  result(2, 1) = m[1][0] * n(0, 1) + m[1][1] * n(1, 1) + m[1][2] * n(2, 1);
-  result(2, 2) = m[2][0] * n(0, 2) + m[2][1] * n(1, 2) + m[2][2] * n(2, 2);
+  result(0, 0) = m[0][0] * n(0, 0) + m[1][0] * n(0, 1) + m[2][0] * n(0, 2);
+  result(0, 1) = m[0][1] * n(1, 0) + m[1][1] * n(1, 1) + m[2][1] * n(1, 2);
+  result(0, 2) = m[0][2] * n(2, 0) + m[1][2] * n(2, 1) + m[2][2] * n(2, 2);
+  result(1, 0) = m[0][0] * n(0, 0) + m[1][0] * n(0, 1) + m[2][0] * n(0, 2);
+  result(1, 1) = m[0][1] * n(1, 0) + m[1][1] * n(1, 1) + m[2][1] * n(1, 2);
+  result(1, 2) = m[0][2] * n(2, 0) + m[1][2] * n(2, 1) + m[2][2] * n(2, 2);
+  result(2, 0) = m[0][0] * n(0, 0) + m[1][0] * n(0, 1) + m[2][0] * n(0, 2);
+  result(2, 1) = m[0][1] * n(1, 0) + m[1][1] * n(1, 1) + m[2][1] * n(1, 2);
+  result(2, 2) = m[0][2] * n(2, 0) + m[1][2] * n(2, 1) + m[2][2] * n(2, 2);
 
   return result;
 }
 
 Matrix3& Matrix3::operator*=(const Matrix3& n)
 {
-  m[0][0] = m[0][0] * n(0, 0) + m[0][1] * n(1, 0) + m[0][2] * n(2, 0);
-  m[0][1] = m[1][0] * n(0, 1) + m[1][1] * n(1, 1) + m[1][2] * n(2, 1);
-  m[0][2] = m[2][0] * n(0, 2) + m[2][1] * n(1, 2) + m[2][2] * n(2, 2);
-  m[1][0] = m[0][0] * n(0, 0) + m[0][1] * n(1, 0) + m[0][2] * n(2, 0);
-  m[1][1] = m[1][0] * n(0, 1) + m[1][1] * n(1, 1) + m[1][2] * n(2, 1);
-  m[1][2] = m[2][0] * n(0, 2) + m[2][1] * n(1, 2) + m[2][2] * n(2, 2);
-  m[2][0] = m[0][0] * n(0, 0) + m[0][1] * n(1, 0) + m[0][2] * n(2, 0);
-  m[2][1] = m[1][0] * n(0, 1) + m[1][1] * n(1, 1) + m[1][2] * n(2, 1);
-  m[2][2] = m[2][0] * n(0, 2) + m[2][1] * n(1, 2) + m[2][2] * n(2, 2);
+  m[0][0] = m[0][0] * n(0, 0) + m[1][0] * n(0, 1) + m[2][0] * n(0, 2);
+  m[0][1] = m[0][1] * n(1, 0) + m[1][1] * n(1, 1) + m[2][1] * n(1, 2);
+  m[0][2] = m[0][2] * n(2, 0) + m[1][2] * n(2, 1) + m[2][2] * n(2, 2);
+  m[1][0] = m[0][0] * n(0, 0) + m[1][0] * n(0, 1) + m[2][0] * n(0, 2);
+  m[1][1] = m[0][1] * n(1, 0) + m[1][1] * n(1, 1) + m[2][1] * n(1, 2);
+  m[1][2] = m[0][2] * n(2, 0) + m[1][2] * n(2, 1) + m[2][2] * n(2, 2);
+  m[2][0] = m[0][0] * n(0, 0) + m[1][0] * n(0, 1) + m[2][0] * n(0, 2);
+  m[2][1] = m[0][1] * n(1, 0) + m[1][1] * n(1, 1) + m[2][1] * n(1, 2);
+  m[2][2] = m[0][2] * n(2, 0) + m[1][2] * n(2, 1) + m[2][2] * n(2, 2);
 
   return *this;
 }
@@ -220,8 +232,12 @@ Matrix3& Matrix3::operator/=(double k)
 
 double Matrix3::determinant(void) const
 {
-  return m[0][0] * m[1][1] * m[2][2] + m[0][1] * m[1][2] * m[2][0] + m[1][0] * m[2][1] * m[0][2] -
-         m[0][2] * m[1][1] * m[2][0] - m[1][1] * m[1][0] * m[2][2] - m[0][0] * m[1][2] * m[2][1];
+  return m[0][0] * m[1][1] * m[2][2] +
+         m[0][1] * m[1][2] * m[2][0] +
+         m[0][2] * m[1][0] * m[2][1] -
+         m[0][2] * m[1][1] * m[2][0] -
+         m[0][1] * m[1][0] * m[2][2] -
+         m[0][0] * m[1][2] * m[2][1];
 }
 
 Matrix3 Matrix3::transpose(void) const
@@ -260,5 +276,417 @@ Matrix3 Matrix3::invert(void) const
   result(2, 2) = (m[0][0] * m[0][1] - m[1][0] * m[1][1]) / det;
 
   return result;
+}
+
+void Matrix3::print(void) const
+{
+  for(int x = 0; x < 3; x++)
+    printf("[ %f, %f, %f ]\n", m[x][0], m[x][1], m[x][2]);
+}
+
+Matrix4::Matrix4(void)
+{
+  m[0][0] = 0;
+  m[0][1] = 0;
+  m[0][2] = 0;
+  m[0][3] = 0;
+  m[1][0] = 0;
+  m[1][1] = 0;
+  m[1][2] = 0;
+  m[1][3] = 0;
+  m[2][0] = 0;
+  m[2][1] = 0;
+  m[2][2] = 0;
+  m[2][3] = 0;
+  m[3][0] = 0;
+  m[3][1] = 0;
+  m[3][2] = 0;
+  m[3][3] = 0;
+}
+
+Matrix4 Matrix4::Identity(void)
+{
+  Matrix4 result;
+
+  result(0, 0) = 1;
+  result(1, 1) = 1;
+  result(2, 2) = 1;
+  result(3, 3) = 1;
+
+  return result;
+}
+
+double& Matrix4::operator()(int x, int y)
+{
+  assert(!(x < 0 || x >= 4 || y < 0 || y >= 4));
+
+  return m[x][y];
+}
+
+double Matrix4::operator()(int x, int y) const
+{
+  assert(!(x < 0 || x >= 4 || y < 0 || y >= 4));
+
+  return m[x][y];
+}
+
+Matrix4 Matrix4::operator-(void) const
+{
+  Matrix4 result;
+
+  result(0, 0) = -m[0][0];
+  result(0, 1) = -m[0][1];
+  result(0, 2) = -m[0][2];
+  result(0, 3) = -m[0][3];
+  result(1, 0) = -m[1][0];
+  result(1, 1) = -m[1][1];
+  result(1, 2) = -m[1][2];
+  result(1, 3) = -m[1][3];
+  result(2, 0) = -m[2][0];
+  result(2, 1) = -m[2][1];
+  result(2, 2) = -m[2][2];
+  result(2, 3) = -m[2][3];
+  result(3, 0) = -m[3][0];
+  result(3, 1) = -m[3][1];
+  result(3, 2) = -m[3][2];
+  result(3, 3) = -m[3][3];
+
+  return result;
+}
+
+Matrix4 Matrix4::operator+(const Matrix4& n) const
+{
+  Matrix4 result;
+
+  result(0, 0) = m[0][0] + n(0, 0);
+  result(0, 1) = m[0][1] + n(0, 1);
+  result(0, 2) = m[0][2] + n(0, 2);
+  result(0, 3) = m[0][3] + n(0, 3);
+  result(1, 0) = m[1][0] + n(1, 0);
+  result(1, 1) = m[1][1] + n(1, 1);
+  result(1, 2) = m[1][2] + n(1, 2);
+  result(1, 3) = m[1][3] + n(1, 3);
+  result(2, 0) = m[2][0] + n(2, 0);
+  result(2, 1) = m[2][1] + n(2, 1);
+  result(2, 2) = m[2][2] + n(2, 2);
+  result(2, 3) = m[2][3] + n(2, 3);
+  result(3, 0) = m[3][0] + n(3, 0);
+  result(3, 1) = m[3][1] + n(3, 1);
+  result(3, 2) = m[3][2] + n(3, 2);
+  result(3, 3) = m[3][3] + n(3, 3);
+
+  return result;
+}
+
+Matrix4& Matrix4::operator+=(const Matrix4& n)
+{
+  m[0][0] += n(0, 0);
+  m[0][1] += n(0, 1);
+  m[0][2] += n(0, 2);
+  m[0][3] += n(0, 3);
+  m[1][0] += n(1, 0);
+  m[1][1] += n(1, 1);
+  m[1][2] += n(1, 2);
+  m[1][3] += n(1, 3);
+  m[2][0] += n(2, 0);
+  m[2][1] += n(2, 1);
+  m[2][2] += n(2, 2);
+  m[2][3] += n(2, 3);
+  m[3][0] += n(3, 0);
+  m[3][1] += n(3, 1);
+  m[3][2] += n(3, 2);
+  m[3][3] += n(3, 3);
+
+  return *this;
+}
+
+Matrix4 Matrix4::operator-(const Matrix4& n) const
+{
+  Matrix4 result;
+
+  result(0, 0) = m[0][0] - n(0, 0);
+  result(0, 1) = m[0][1] - n(0, 1);
+  result(0, 2) = m[0][2] - n(0, 2);
+  result(0, 3) = m[0][3] - n(0, 3);
+  result(1, 0) = m[1][0] - n(1, 0);
+  result(1, 1) = m[1][1] - n(1, 1);
+  result(1, 2) = m[1][2] - n(1, 2);
+  result(1, 3) = m[1][3] - n(1, 3);
+  result(2, 0) = m[2][0] - n(2, 0);
+  result(2, 1) = m[2][1] - n(2, 1);
+  result(2, 2) = m[2][2] - n(2, 2);
+  result(2, 3) = m[2][3] - n(2, 3);
+  result(3, 0) = m[3][0] - n(3, 0);
+  result(3, 1) = m[3][1] - n(3, 1);
+  result(3, 2) = m[3][2] - n(3, 2);
+  result(3, 3) = m[3][3] - n(3, 3);
+
+  return result;
+}
+
+Matrix4& Matrix4::operator-=(const Matrix4& n)
+{
+  m[0][0] -= n(0, 0);
+  m[0][1] -= n(0, 1);
+  m[0][2] -= n(0, 2);
+  m[0][3] -= n(0, 3);
+  m[1][0] -= n(1, 0);
+  m[1][1] -= n(1, 1);
+  m[1][2] -= n(1, 2);
+  m[1][3] -= n(1, 3);
+  m[2][0] -= n(2, 0);
+  m[2][1] -= n(2, 1);
+  m[2][2] -= n(2, 2);
+  m[2][3] -= n(2, 3);
+  m[3][0] -= n(3, 0);
+  m[3][1] -= n(3, 1);
+  m[3][2] -= n(3, 2);
+  m[3][3] -= n(3, 3);
+
+  return *this;
+}
+
+Matrix4 Matrix4::operator*(double k) const
+{
+  Matrix4 result;
+
+  result(0, 0) = m[0][0] * k;
+  result(0, 1) = m[0][1] * k;
+  result(0, 2) = m[0][2] * k;
+  result(0, 3) = m[0][3] * k;
+  result(1, 0) = m[1][0] * k;
+  result(1, 1) = m[1][1] * k;
+  result(1, 2) = m[1][2] * k;
+  result(1, 3) = m[1][3] * k;
+  result(2, 0) = m[2][0] * k;
+  result(2, 1) = m[2][1] * k;
+  result(2, 2) = m[2][2] * k;
+  result(2, 3) = m[2][3] * k;
+  result(3, 0) = m[3][0] * k;
+  result(3, 1) = m[3][1] * k;
+  result(3, 2) = m[3][2] * k;
+  result(3, 3) = m[3][3] * k;
+
+  return result;
+}
+
+Matrix4& Matrix4::operator*=(double k)
+{
+  m[0][0] *= k;
+  m[0][1] *= k;
+  m[0][2] *= k;
+  m[0][3] *= k;
+  m[1][0] *= k;
+  m[1][1] *= k;
+  m[1][2] *= k;
+  m[1][3] *= k;
+  m[2][0] *= k;
+  m[2][1] *= k;
+  m[2][2] *= k;
+  m[2][3] *= k;
+  m[3][0] *= k;
+  m[3][1] *= k;
+  m[3][2] *= k;
+  m[3][3] *= k;
+
+  return *this;
+}
+
+Matrix4 Matrix4::operator*(const Matrix4& n) const
+{
+  Matrix4 r;
+
+  r(0, 0) = m[0][0] * n(0, 0) + m[1][0] * n(0, 1) + m[2][0] * n(0, 2) + m[3][0] * n(0, 3);
+  r(0, 1) = m[0][1] * n(0, 0) + m[1][1] * n(0, 1) + m[2][1] * n(0, 2) + m[3][1] * n(0, 3);
+  r(0, 2) = m[0][2] * n(0, 0) + m[1][2] * n(0, 1) + m[2][2] * n(0, 2) + m[3][2] * n(0, 3);
+  r(0, 3) = m[0][3] * n(0, 0) + m[1][3] * n(0, 1) + m[2][3] * n(0, 2) + m[3][3] * n(0, 3);
+  r(1, 0) = m[0][0] * n(1, 0) + m[1][0] * n(1, 1) + m[2][0] * n(1, 2) + m[3][0] * n(1, 3);
+  r(1, 1) = m[0][1] * n(1, 0) + m[1][1] * n(1, 1) + m[2][1] * n(1, 2) + m[3][1] * n(1, 3);
+  r(1, 2) = m[0][2] * n(1, 0) + m[1][2] * n(1, 1) + m[2][2] * n(1, 2) + m[3][2] * n(1, 3);
+  r(1, 3) = m[0][3] * n(1, 0) + m[1][3] * n(1, 1) + m[2][3] * n(1, 2) + m[3][3] * n(1, 3);
+  r(2, 0) = m[0][0] * n(2, 0) + m[1][0] * n(2, 1) + m[2][0] * n(2, 2) + m[3][0] * n(2, 3);
+  r(2, 1) = m[0][1] * n(2, 0) + m[1][1] * n(2, 1) + m[2][1] * n(2, 2) + m[3][1] * n(2, 3);
+  r(2, 2) = m[0][2] * n(2, 0) + m[1][2] * n(2, 1) + m[2][2] * n(2, 2) + m[3][2] * n(2, 3);
+  r(2, 3) = m[0][3] * n(2, 0) + m[1][3] * n(2, 1) + m[2][3] * n(2, 2) + m[3][3] * n(2, 3);
+  r(3, 0) = m[0][0] * n(3, 0) + m[1][0] * n(3, 1) + m[2][0] * n(3, 2) + m[3][0] * n(3, 3);
+  r(3, 1) = m[0][1] * n(3, 0) + m[1][1] * n(3, 1) + m[2][1] * n(3, 2) + m[3][1] * n(3, 3);
+  r(3, 2) = m[0][2] * n(3, 0) + m[1][2] * n(3, 1) + m[2][2] * n(3, 2) + m[3][2] * n(3, 3);
+  r(3, 3) = m[0][3] * n(3, 0) + m[1][3] * n(3, 1) + m[2][3] * n(3, 2) + m[3][3] * n(3, 3);
+
+  return r;
+}
+
+Matrix4& Matrix4::operator*=(const Matrix4& n)
+{
+  m[0][0] = m[0][0] * n(0, 0) + m[1][0] * n(0, 1) + m[2][0] * n(0, 2) + m[3][0] * n(0, 3);
+  m[0][1] = m[0][1] * n(0, 0) + m[1][1] * n(0, 1) + m[2][1] * n(0, 2) + m[3][1] * n(0, 3);
+  m[0][2] = m[0][2] * n(0, 0) + m[1][2] * n(0, 1) + m[2][2] * n(0, 2) + m[3][2] * n(0, 3);
+  m[0][3] = m[0][3] * n(0, 0) + m[1][3] * n(0, 1) + m[2][3] * n(0, 2) + m[3][3] * n(0, 3);
+  m[1][0] = m[0][0] * n(1, 0) + m[1][0] * n(1, 1) + m[2][0] * n(1, 2) + m[3][0] * n(1, 3);
+  m[1][1] = m[0][1] * n(1, 0) + m[1][1] * n(1, 1) + m[2][1] * n(1, 2) + m[3][1] * n(1, 3);
+  m[1][2] = m[0][2] * n(1, 0) + m[1][2] * n(1, 1) + m[2][2] * n(1, 2) + m[3][2] * n(1, 3);
+  m[1][3] = m[0][3] * n(1, 0) + m[1][3] * n(1, 1) + m[2][3] * n(1, 2) + m[3][3] * n(1, 3);
+  m[2][0] = m[0][0] * n(2, 0) + m[1][0] * n(2, 1) + m[2][0] * n(2, 2) + m[3][0] * n(2, 3);
+  m[2][1] = m[0][1] * n(2, 0) + m[1][1] * n(2, 1) + m[2][1] * n(2, 2) + m[3][1] * n(2, 3);
+  m[2][2] = m[0][2] * n(2, 0) + m[1][2] * n(2, 1) + m[2][2] * n(2, 2) + m[3][2] * n(2, 3);
+  m[2][3] = m[0][3] * n(2, 0) + m[1][3] * n(2, 1) + m[2][3] * n(2, 2) + m[3][3] * n(2, 3);
+  m[3][0] = m[0][0] * n(3, 0) + m[1][0] * n(3, 1) + m[2][0] * n(3, 2) + m[3][0] * n(3, 3);
+  m[3][1] = m[0][1] * n(3, 0) + m[1][1] * n(3, 1) + m[2][1] * n(3, 2) + m[3][1] * n(3, 3);
+  m[3][2] = m[0][2] * n(3, 0) + m[1][2] * n(3, 1) + m[2][2] * n(3, 2) + m[3][2] * n(3, 3);
+  m[3][3] = m[0][3] * n(3, 0) + m[1][3] * n(3, 1) + m[2][3] * n(3, 2) + m[3][3] * n(3, 3);
+
+  return *this;
+}
+
+Matrix4 Matrix4::operator/(double k) const
+{
+  Matrix4 result;
+
+  result(0, 0) = m[0][0] / k;
+  result(0, 1) = m[0][1] / k;
+  result(0, 2) = m[0][2] / k;
+  result(0, 3) = m[0][3] / k;
+  result(1, 0) = m[1][0] / k;
+  result(1, 1) = m[1][1] / k;
+  result(1, 2) = m[1][2] / k;
+  result(1, 3) = m[1][3] / k;
+  result(2, 0) = m[2][0] / k;
+  result(2, 1) = m[2][1] / k;
+  result(2, 2) = m[2][2] / k;
+  result(2, 3) = m[2][3] / k;
+  result(3, 0) = m[3][0] / k;
+  result(3, 1) = m[3][1] / k;
+  result(3, 2) = m[3][2] / k;
+  result(3, 3) = m[3][3] / k;
+
+  return result;
+}
+
+Matrix4& Matrix4::operator/=(double k)
+{
+  m[0][0] /= k;
+  m[0][1] /= k;
+  m[0][2] /= k;
+  m[0][3] /= k;
+  m[1][0] /= k;
+  m[1][1] /= k;
+  m[1][2] /= k;
+  m[1][3] /= k;
+  m[2][0] /= k;
+  m[2][1] /= k;
+  m[2][2] /= k;
+  m[2][3] /= k;
+  m[3][0] /= k;
+  m[3][1] /= k;
+  m[3][2] /= k;
+  m[3][3] /= k;
+
+  return *this;
+}
+
+Matrix3 Matrix4::submatrix(int x, int y) const
+{
+  Matrix3 result;
+
+  int x_prime = -1;
+  int y_prime;
+
+  for(int i = 0; i < 4; i++) {
+    if(i == x)
+      continue;
+
+    x_prime++;
+    y_prime = -1;
+
+    for(int j = 0; j < 4; j++) {
+      if(j == y)
+        continue;
+
+      y_prime++;
+
+      result(x_prime, y_prime) = m[i][j];
+    }
+  }
+
+  return result;
+}
+
+double Matrix4::subdeterminant(int x, int y) const
+{
+  return submatrix(x, y).determinant();
+}
+
+double Matrix4::determinant(void) const
+{
+  double result = 0;
+
+  if(m[0][0] != 0)
+    result += m[0][0] * subdeterminant(0, 0);
+  if(m[0][1] != 0)
+    result -= m[0][1] * subdeterminant(0, 1);
+  if(m[0][2] != 0)
+    result += m[0][2] * subdeterminant(0, 2);
+  if(m[0][3] != 0)
+    result -= m[0][3] * subdeterminant(0, 3);
+
+  return result;
+}
+
+Matrix4 Matrix4::transpose(void) const
+{
+  Matrix4 result;
+
+  result(0, 0) = m[0][0];
+  result(0, 1) = m[1][0];
+  result(0, 2) = m[2][0];
+  result(0, 3) = m[3][0];
+  result(1, 0) = m[0][1];
+  result(1, 1) = m[1][1];
+  result(1, 2) = m[2][1];
+  result(1, 3) = m[3][1];
+  result(2, 0) = m[0][2];
+  result(2, 1) = m[1][2];
+  result(2, 2) = m[2][2];
+  result(2, 3) = m[3][2];
+  result(3, 0) = m[0][3];
+  result(3, 1) = m[1][3];
+  result(3, 2) = m[2][3];
+  result(3, 3) = m[3][3];
+
+  return result;
+}
+
+Matrix4 Matrix4::invert(void) const
+{
+  Matrix4 result;
+
+  double det = determinant();
+
+  assert(det != 0);
+
+  result(0, 0) = subdeterminant(0, 0) / det;
+  result(0, 1) = subdeterminant(0, 1) / det;
+  result(0, 2) = subdeterminant(0, 2) / det;
+  result(0, 3) = subdeterminant(0, 3) / det;
+  result(1, 0) = subdeterminant(1, 0) / det;
+  result(1, 1) = subdeterminant(1, 1) / det;
+  result(1, 2) = subdeterminant(1, 2) / det;
+  result(1, 3) = subdeterminant(1, 3) / det;
+  result(2, 0) = subdeterminant(2, 0) / det;
+  result(2, 1) = subdeterminant(2, 1) / det;
+  result(2, 2) = subdeterminant(2, 2) / det;
+  result(2, 3) = subdeterminant(2, 3) / det;
+  result(3, 0) = subdeterminant(3, 0) / det;
+  result(3, 1) = subdeterminant(3, 1) / det;
+  result(3, 2) = subdeterminant(3, 2) / det;
+  result(3, 3) = subdeterminant(3, 3) / det;
+
+  return result;
+}
+
+void Matrix4::print(void) const
+{
+  for(int x = 0; x < 4; x++)
+    printf("[ %f, %f, %f, %f ]\n", m[x][0], m[x][1], m[x][2], m[x][3]);
 }
 
--- src/Matrix.h
+++ src/Matrix.h
@@ -9,6 +9,8 @@ class Matrix3
   public:
     Matrix3(void);
 
+    static Matrix3 Identity(void);
+
     double& operator()(int x, int y);
     double operator()(int x, int y) const;
     Matrix3 operator-(void) const;
@@ -27,6 +29,8 @@ class Matrix3
     double determinant(void) const;
     Matrix3 transpose(void) const;
     Matrix3 invert(void) const;
+
+    void print(void) const;
 };
 
 class Matrix4
@@ -35,6 +39,8 @@ class Matrix4
     double m[4][4];
   public:
     Matrix4(void);
+
+    static Matrix4 Identity(void);
 
     double& operator()(int x, int y);
     double operator()(int x, int y) const;
@@ -49,5 +55,13 @@ class Matrix4
     Matrix4& operator*=(const Matrix4& n);
     Matrix4 operator/(double k) const;
     Matrix4& operator/=(double k);
+
+    Matrix3 submatrix(int x, int y) const;
+    double subdeterminant(int x, int y) const;
+    double determinant(void) const;
+    Matrix4 transpose(void) const;
+    Matrix4 invert(void) const;
+
+    void print(void) const;
 };
 
