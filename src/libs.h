7723eb086ee453d86d6366f706570ef60ced7232
Anthony Burzillo <burz@jhu.edu>
2013-12-25 20:46:07 -0500

    Keep function names same accross OS's

--- src/libs.h
+++ src/libs.h
@@ -2,6 +2,10 @@
 
 #ifdef __APPLE__
   #include <GLUT/glut.h>
+  #define glBindVertexArray glBindVertexArrayAPPLE 
+  #define glGenVertexArrays glGenVertexArraysAPPLE
+  #define glGenerateMipmap glGenerateMipmapEXT
+  #define glDeleteVertexArrays glDeleteVertexArraysAPPLE
 #else
   #include <GL/glut.h>
 #endif
